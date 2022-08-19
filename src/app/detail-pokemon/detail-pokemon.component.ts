6ec5f2ade53522e6ef31437db27812e7fa57b2d0
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-19 14:09:18 +0200

    récupérer l'id

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemonList: Pokemon[];
+  /*propriété qui contient un pokémon pour l'utilisateur ou undefined (message à afficher =>ngif à gérer)*/
+  pokemon:Pokemon|undefined;
 
-  ngOnInit(): void {
+  //Service activatedRoute injecté dans le composant via le constructeur
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    //on récupère l'id contenu dans l'url (soit une string soit null)
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    /*si mon id a bien été trouvé dans l'url, j'attribue a la propriété pokémon, le pokemon qui correspond à cet identifiant*/
+    if(pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon=> pokemon.id == +pokemonId)
+    }
   }
-
 }
