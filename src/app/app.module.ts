b63ee4762ea0b9e253a79cf7bbabc7320fd37967
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-19 15:16:24 +0200

    on fait attention à l'ordre des imports des modules : les routes s'exécutent dans l'ordre DONC les plus complexes d'abord et les plus simples à la fin donc ici l'import de AppRoutingModule après le PokemonModule dans app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PokemonModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
