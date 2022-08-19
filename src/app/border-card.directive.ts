3f9f22c52be42cd23981cbef3e86c0bf4954c01a
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-19 12:16:49 +0200

    propriétés stockées par défaut

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -4,22 +4,27 @@ import { Directive,ElementRef, HostListener, Input } from '@angular/core';
   selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
+
+  private initialColor: string = '#f5f5f5';
+  private defaultColor:string = '#009688';
+  private defaultHeight: number = 200 ;
  //on initialise nos fonctions avec des valeurs de bases 
   constructor(private el:ElementRef) { 
 
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
   }
   //@Input permet que la couleur soit personnalisable dans le tpl
   @Input('pkmnBorderCard') borderColor: string; //alias
-  @Input() pkmnBorderCard: string; //sans alias
+
 
   //on ajoute des  "eventListeners" qui vont réagir en fonction de méthodes
   @HostListener('mouseenter') onMouseEnter() {
     // si l'utilisateur définit une couleur OU celle par défaut 
-    this.setBorder(this.borderColor || '#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
   
   //notre fonction
@@ -27,5 +32,8 @@ export class BorderCardDirective {
   private setBorder(color: string) {
     let border = 'solid 4px' + color;
     this.el.nativeElement.style.border = border;
+  }
+  private setHeight(height:number) {
+    this.el.nativeElement.style.height = height + 'px';
   }
 }
