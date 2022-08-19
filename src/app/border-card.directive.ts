e661857f57eea6b4f4163cde5995ee95db7fe3dc
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-19 11:59:58 +0200

    ajout d'une directive

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appBorderCard]'
+  selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
- 
-  constructor() { }
+ //on initialise nos fonctions avec des valeurs de bases 
+  constructor(private el:ElementRef) { 
 
+    this.setBorder('#f5f5f5');
+  }
+  //on ajoute des  "eventListeners" qui vont réagir en fonction de méthodes
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder('#009688');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+  }
+  
+  //nos deux fonctions
+
+  private setBorder(color: string) {
+    let border = 'solid 4px' + color;
+    this.el.nativeElement.style.border = border;
+  }
 }
