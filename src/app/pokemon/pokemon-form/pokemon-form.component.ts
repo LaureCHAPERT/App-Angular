3e6cb7f7cc9c0be59f246e28b0566ac8c8484d27
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-20 16:33:53 +0200

    ajout de règles logique pour réagir aux events dans component.ts

---
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-form',
+  templateUrl: './pokemon-form.component.html',
+})
+export class PokemonFormComponent implements OnInit {
+  //avec cette ligne on demande une instanciation de Pokemon
+  @Input() pokemon:Pokemon;
+  types: string[];
+
+  constructor(
+    private router:Router,
+    private pokemonService: PokemonService
+  ) { }
+
+  //on initialise tous les types présents dans le projet
+  ngOnInit()  {
+    this.types= this.pokemonService.getPokemonTypeList();
+  }
+  //on vérifie si le pokémon en question a un type ou non 
+  //afin de pré-cocher ou non un form
+  hasType(type:string):boolean {
+    return this.pokemon.types.includes(type);
+  }
+  //vérifier si le type sélectionné dans le form est déjà présent ou non
+  selectType($event: Event, type:string) {
+    // le user a t'il coché la case ? 
+    const isChecked:boolean = ($event.target as HTMLInputElement).checked;
+    //si oui j'ajoute le type
+    if(isChecked) {
+      this.pokemon.types.push(type);
+      //sinon je l'enlève
+    }else {
+      //
+      const index = this.pokemon.types.indexOf(type);
+      this.pokemon.types.splice(index, 1);
+    }
+  }
+  onSubmit() {
+    console.log('Submit Form');
+    this.router.navigate(['pokemon', this.pokemon.id]);
+  }
+}
