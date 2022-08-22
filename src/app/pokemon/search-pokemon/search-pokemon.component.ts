0bda32d9f214d0089405eb445a5b17035e914da5
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-22 10:34:26 +0200

    moteur de recherche fonctionnem

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-search-pokemon',
@@ -14,9 +15,24 @@ export class SearchPokemonComponent implements OnInit {
   // recherches => {...pokemonList(a)...pokemonList(ab)....}
   pokemons$: Observable<Pokemon[]>;
 
-  constructor(private router:Router) { }
+  constructor(
+    private router:Router,
+    private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      /*on attend un certain temps pour éviter les appels successifs
+      /alors que l'utilisateur n'a pas fini de taper*/
+      debounceTime(300),
+      /*on attend qu'il y ait un changement dans les recherches
+      on ne lance pas deux fois une recherche pour "ab" */
+      distinctUntilChanged(),
+      switchMap((term)=> this.pokemonService.searchPokemonList(term))
+      /*concatMap / mergeMap /switchMap 
+        switchMap => annuler la dernière recherche si elle est en cours
+        et la rempplacer par la dernière
+        on ne va pas obtenir {.....Observable<"abc>"....mais pokemonList(ab)}*/
+    )
   }
   //à chaque fois que l'utilisateur va taper un caractère on appelle cette méthode
   //et on pousse son terme dans le flux de données Subject
